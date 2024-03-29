595a99c6b570852bc8ee907d29889546fe3de055
ArielSurco <arielsurcoa@gmail.com>
2024-03-29 13:14:42 -0300

    chore: remove dist folder and add it on git ignore

--- dist/server/Server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Server = void 0;
-class Server {
-    init() {
-        console.log('Server started');
-    }
-}
-exports.Server = Server;
